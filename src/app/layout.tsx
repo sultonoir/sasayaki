bd1f73cd55c7ac269ef32dbb416ea3a945d6a0cb
Sulton <130794606+sultonoir@users.noreply.github.com>
2025-03-28 04:26:26 +0700

    ref : change directory

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,14 +1,8 @@
+import "@/styles/globals.css";
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
+import { Inter } from "next/font/google";
 
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
+const interSans = Inter({
   subsets: ["latin"],
 });
 
@@ -23,12 +17,10 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-      >
-        {children}
-      </body>
+    <html
+      lang="en"
+      suppressHydrationWarning>
+      <body className={`${interSans.className} antialiased`}>{children}</body>
     </html>
   );
 }
